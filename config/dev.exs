acfa70f05259034899bccdd9221a466a4511f4a5
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-02-05 22:09:21 +0300

    PG setup

--- config/dev.exs
+++ config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :pmj, Pmj.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   database: "pmj_dev",
   hostname: "localhost",
   pool_size: 10
